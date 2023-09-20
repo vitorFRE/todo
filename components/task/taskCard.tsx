779f0ea7e3aaf4f4c21b52676003e55016f091b3
Vitor <frete497@gmail.com>
2023-09-20 18:21:10 -0400

    feat: update next to 13.5

--- components/task/taskCard.tsx
+++ components/task/taskCard.tsx
@@ -8,8 +8,8 @@ interface TaskCardProps {
 
 export const TaskCard: React.FC<TaskCardProps> = ({ title, status, description }) => {
 	return (
-		<div className='max-w-full sm:max-w-[304px] min-h-[142px] py-4 px-4 border border-black/60 dark:border-white/60 rounded-md'>
-			<div className='flex items-center justify-between gap-3'>
+		<div className='max-w-full sm:max-w-[304px] min-h-[142px] py-4 px-4 border border-black/60 dark:border-white/60 rounded-md '>
+			<div className='flex item justify-between gap-3'>
 				<h3 className='font-medium text-left'>{title}</h3>
 				<Badge className={status ? 'bg-green-400 hover:bg-green-200' : ''}>
 					{status ? 'Concluído' : 'Pendente'}
